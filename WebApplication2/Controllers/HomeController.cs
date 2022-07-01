3303019dfaa7cc2cd380506a55ebe79fb04f4b9a
parvathi D <parvathid@zirafftechnologies.com>
2022-07-01 16:20:05 +0530

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult GetData()
+        {
+            List<GetData> lstfruits = new List<GetData>();
+            GetData fruits = new();
+            fruits.Name = "Mango";
+            fruits.Colour = "Green";
+            lstfruits.Add(fruits);
+            GetData fruits1 =new();
+            fruits1.Name = "Apple";
+            fruits1.Colour = "Red";
+            lstfruits.Add(fruits1);
+            return View(lstfruits);
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ WebApplication2/Models/GetData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class GetData
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+    }
+}
